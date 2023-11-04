3d8402064029e1d4c9c1980f4dc527cc43cfb93d
Artjom Kurapov <artjom.kurapov@clarifai.com>
2023-11-05 01:26:05 +0200

    fix

--- restart.sh
+++ restart.sh
@@ -1,6 +1,3 @@
 cd /www/satellite-pollination-map/
 COMPOSE_PROJECT_NAME=gratheon docker-compose down
-
-rm -rf /www/satellite-pollination-map/
-
 COMPOSE_PROJECT_NAME=gratheon docker-compose up -d --build
